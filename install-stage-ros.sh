c934136fe0916e97e4906410d28ff320f171a4ea
slremy <sekouremy@gmail.com>
2015-11-30 02:44:15 -0500

    Update install-stage-ros.sh

--- install-stage-ros.sh
+++ install-stage-ros.sh
@@ -2,13 +2,13 @@
 sudo cp /etc/apt/sources.list  /etc/apt/sources.list.backup
 sudo sed -i "/^# deb.*multiverse/ s/^# //" /etc/apt/sources.list 
 #instructions for 14.04 (Trusty)
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
-wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
 sudo apt-get -y update
 sudo apt-get -y install xvfb x11-apps screen
 sudo apt-get -y install gdb
 sudo apt-get -y install python-rosdistro python-rosdep python-rosinstall python-rosinstall-generator
-sudo apt-get -y install ros-indigo-rosbash
+sudo apt-get -y install ros-indigo-ros-base ros-indigo-tf
 sudo apt-get -y install ros-indigo-mjpeg-server
 sudo apt-get -y install ros-indigo-stage-ros
 sudo apt-get -y install ros-indigo-angles
