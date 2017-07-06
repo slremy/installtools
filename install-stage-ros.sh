f62f9988db66c966cb32f0af5fc191a42418bcbe
slremy <sekouremy@gmail.com>
2017-07-06 12:10:19 +0300

    Update install-stage-ros.sh
    Updating to reflect the Xenial and Lunar

--- install-stage-ros.sh
+++ install-stage-ros.sh
@@ -1,21 +1,19 @@
 #install ROS (and Stage)
 sudo cp /etc/apt/sources.list  /etc/apt/sources.list.backup
 sudo sed -i "/^# deb.*multiverse/ s/^# //" /etc/apt/sources.list 
-#instructions for 14.04 (Trusty)
+#instructions for 16.04 (Xenial)
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get -y update
 sudo apt-get -y install xvfb x11-apps screen
 sudo apt-get -y install gdb
-sudo apt-get -y install python-rosdistro python-rosdep python-rosinstall python-rosinstall-generator
-sudo apt-get -y install ros-indigo-ros-base ros-indigo-tf
-sudo apt-get -y install ros-indigo-web-video-server
-sudo apt-get -y install ros-indigo-stage
-sudo apt-get -y install ros-indigo-stage-ros
-sudo apt-get -y install ros-indigo-angles
+sudo apt-get -y install python-rosinstall
+sudo apt-get -y install ros-lunar-ros-base
+sudo apt-get -y install ros-lunar-stage-ros
+#sudo apt-get -y install ros-lunar-angles
 sudo rosdep init
 rosdep update
-echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/lunar/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
 #install ROS web service manually
@@ -30,12 +28,18 @@ chmod 755 ros_web_service/src/*py
 cd ~/catkin_ws/
 catkin_make
 cd ~/catkin_ws/src/
-git clone https://github.com/slremy/stage_ros.git 
+sudo apt-get -y install ros-lunar-cv-bridge ros-lunar-image-transport
+git clone https://github.com/GT-RAIL/async_web_server_cpp.git
+git clone https://github.com/RobotWebTools/web_video_server.git
 cd ~/catkin_ws/
 catkin_make
+#cd ~/catkin_ws/src/
+#git clone https://github.com/slremy/stage_ros.git 
+#cd ~/catkin_ws/
+#catkin_make
 source ~/.bashrc
 
-sudo apt-get -y install python-webpy
+pip install web.py
 
 #screen
 #Xvfb :1 -screen 0 1024x768x24 2>&1 >/dev/null &
